505e1f566d755aa7e8a864442c92605ef3b3777e
Henry Tseng <rabbit.henry@gmail.com>
2023-06-29 14:47:26 +0800

    Initial commit

---
+++ Gogorent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gogorent
+//
+//  Created by Henry Tseng on 2023/6/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
