0784b773fc32ec08426a2bbd4c1a8dd30197ea23
alexlin <>
2023-06-29 17:52:15 +0800

    Add loading animation

--- Gogorent/Cells/ResultCell.swift
+++ Gogorent/Cells/ResultCell.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ResultCell: UITableViewCell {
     
+    private var loadingView: UIView!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
+    
+    private(set) var isLoading: Bool = false
     
     var imageNames: [String] = [] {
         didSet {
@@ -23,6 +26,16 @@ class ResultCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
+        
+        loadingView = UIView()
+        let resultLableContentView = resultLabel.superview!
+        resultLableContentView.superview?.addSubview(loadingView)
+        NSLayoutConstraint.activate([
+            loadingView.topAnchor.constraint(equalTo: resultLableContentView.topAnchor),
+            loadingView.leadingAnchor.constraint(equalTo: resultLableContentView.leadingAnchor),
+            loadingView.bottomAnchor.constraint(equalTo: resultLableContentView.bottomAnchor),
+            loadingView.trailingAnchor.constraint(equalTo: resultLableContentView.trailingAnchor)
+        ])
         
         resultLabel.textColor = .white
     }
@@ -62,6 +75,43 @@ class ResultCell: UITableViewCell {
                 imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
             stackView.addArrangedSubview(view)
+        }
+    }
+    
+    // baseline = to put the bottom of the dots at the baseline of the text in the label
+    // dotXOffset = gap between end of label and first dot
+    // dotSize = dot width and height
+    // dotSpacing = gap between dots
+    private func showAnimatingDotsInImageView(dotsView: UIView, baseline: CGFloat, dotXOffset: CGFloat, dotSize: CGFloat, dotSpacing: CGFloat) {
+        let lay = CAReplicatorLayer()
+        let bar = CALayer()
+        bar.frame = CGRect(x: dotXOffset, y: baseline - dotSize, width: dotSize, height: dotSize)
+        bar.cornerRadius = bar.frame.width / 2  // we want round dots
+        bar.backgroundColor = UIColor.black.cgColor
+        lay.addSublayer(bar)
+        lay.instanceCount = 3   //How many instances / objs do you want to see
+        lay.instanceTransform = CATransform3DMakeTranslation(dotSpacing, 0, 0) //1st arg is the spacing between the instances
+        let anim = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        anim.fromValue = 1.0
+        anim.toValue = 0.2
+        anim.duration = 1
+        anim.repeatCount = .infinity
+        bar.add(anim, forKey: nil)
+        lay.instanceDelay = anim.duration / Double(lay.instanceCount)
+        dotsView.layer.addSublayer(lay)    // add to the view
+    }
+    
+    func playLoadingAnimation() {
+        loadingView.alpha = 1.0
+        resultLabel.superview?.alpha = 0.0
+        stackView.alpha = 0.0
+        
+        showAnimatingDotsInImageView(dotsView: loadingView, baseline: 0.0, dotXOffset: 0.0, dotSize: 8.0, dotSpacing: 12.0)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [self] in
+            loadingView.alpha = 0.0
+            resultLabel.superview?.alpha = 1.0
+            stackView.alpha = 1.0
         }
     }
 }
